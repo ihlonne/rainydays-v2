557af2ed3240aa1c7834a202ffbe123ae0d22efd
ihlonne <88430256+ihlonne@users.noreply.github.com>
2024-03-26 13:19:37 +0100

    add scroll on navigation js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+'use strict';
+
+// Hamburger menu
 const hamburger = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.navigation-links');
 
@@ -7,3 +10,14 @@ const mobileMenu = () => {
 };
 
 hamburger.addEventListener('click', mobileMenu);
+
+// Change background color of navigation bar on scroll
+const header = document.querySelector('.navigation');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        header.classList.add('change');
+    } else {
+        header.classList.remove('change');
+    }
+});
