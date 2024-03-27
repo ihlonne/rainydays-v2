e6887792bfe7509264051d5ab64ff590d5f4aa68
ihlonne <88430256+ihlonne@users.noreply.github.com>
2024-03-27 10:18:18 +0100

    add scroll effect on products

--- src/js/script.js
+++ src/js/script.js
@@ -21,3 +21,31 @@ window.addEventListener('scroll', () => {
         header.classList.remove('change');
     }
 });
+
+// Carousel effect on products on landing page
+const carousel = document.querySelector('.carousel');
+
+let isDragging = false,
+    startX,
+    startScrollLeft;
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add('dragging');
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+    if (!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove('dragging');
+};
+
+carousel.addEventListener('mousedown', dragStart);
+carousel.addEventListener('mousemove', dragging);
+document.addEventListener('mouseup', dragStop);
