078babe2c84211f1e44283d14d3db6930120af64
ihlonne <88430256+ihlonne@users.noreply.github.com>
2024-03-27 10:17:33 +0100

    update class names in script

--- src/js/api.js
+++ src/js/api.js
@@ -5,6 +5,8 @@ async function loadInfo() {
     return data;
 }
 
+export default { loadInfo };
+
 document.addEventListener('DOMContentLoaded', async () => {
     let data = [];
     try {
@@ -14,5 +16,38 @@ document.addEventListener('DOMContentLoaded', async () => {
         console.log(e);
     }
 
-    console.log(data.image[0]);
+    const productWrapperFemale = document.querySelector(
+        '.women-wrapper__products'
+    );
+    const productWrapperMale = document.querySelector('.men-wrapper__products');
+
+    data.data.map((item) => {
+        const productArticle = document.createElement('article');
+        productArticle.setAttribute('id', item.id);
+        productArticle.classList.add('.products-wrapper__item');
+        productArticle.classList.add('.card');
+        const content = `
+        <a href="">
+            <div class="product__img">
+            <img src="${item.image.url}" alt="${item.description}" draggable="false" >
+            </div>
+            <div class="products-product__info">
+                <h3>${item.title}</h3>
+                <p>${item.price}</p>
+            </div>
+        </a>
+        `;
+        productArticle.innerHTML = content;
+
+        // Check if item is categorized as male or female
+
+        if (item.gender === 'Female') {
+            productWrapperFemale.append(productArticle);
+        } else {
+            productWrapperMale.append(productArticle);
+        }
+    });
+
+    console.log(data.data[0]);
+    return data;
 });
