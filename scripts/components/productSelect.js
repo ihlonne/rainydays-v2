79757622bf28b51be821c862e404760611777137
ihlonne <88430256+ihlonne@users.noreply.github.com>
2024-04-21 11:07:13 +0200

    delete unnecessary files

--- scripts/components/productSelect.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const selectBtn = document.querySelector('.select select');
-
-export const changeSelectOption = () => {
-  selectBtn.addEventListener('click', console.log('hello'));
-};
