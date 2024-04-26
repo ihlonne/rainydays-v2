d74d50fdfeaddf861d266562e476892eafac1a65
ihlonne <88430256+ihlonne@users.noreply.github.com>
2024-04-26 20:37:58 +0200

    create handleUI function

---
+++ scripts/ui/common/handleUI.mjs
@@ -0,0 +1,23 @@
+export const handleUI = function () {
+  // Hamburger menu
+  const hamburger = document.querySelector('.hamburger-menu');
+  const navMenu = document.querySelector('.navigation-links');
+
+  const mobileMenu = () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  };
+
+  hamburger.addEventListener('click', mobileMenu);
+
+  // Change background color of navigation bar on scroll
+  const header = document.querySelector('.navigation');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      header.classList.add('change');
+    } else {
+      header.classList.remove('change');
+    }
+  });
+};
