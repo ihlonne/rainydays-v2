78259133f78255ddbf618d9cd7c5b4af74e6fc8d
ihlonne <88430256+ihlonne@users.noreply.github.com>
2024-04-26 20:39:33 +0200

    update file paths

--- scripts/scripts.mjs
+++ scripts/scripts.mjs
@@ -1,19 +1,22 @@
 import { API_RAINY_DAYS } from './common/constants.mjs';
 import { fetchData } from './utils/fetchData.mjs';
 
-import { hideLoader, showLoader } from './loader.mjs';
-import setCategory from './selectOption.mjs';
+import { hideLoader, showLoader } from './ui/common/loader.mjs';
+import setCategory from './ui/jackets/setCategory.mjs';
+import { handleUI } from './ui/common/handleUI.mjs';
 
 export async function main() {
+  handleUI();
+
   try {
     showLoader();
-    const rainyDaysData = await fetchData(API_RAINY_DAYS);
-    setCategory(rainyDaysData);
 
-    return rainyDaysData;
+    const jackets = await fetchData(API_RAINY_DAYS);
+
+    setCategory(jackets);
   } catch (error) {
     console.log(error);
-    throw new Error();
+    //display error message in HTML
   } finally {
     hideLoader();
   }
