575f477ec70e9f4d8684379f7dae8fc6b080b274
ihlonne <88430256+ihlonne@users.noreply.github.com>
2024-05-04 19:26:41 +0200

    add displayMessge in try/catch

--- scripts/ui/jackets/displayJackets.mjs
+++ scripts/ui/jackets/displayJackets.mjs
@@ -1,25 +1,32 @@
+import { displayMessage } from '../common/displayMessage.mjs';
+
 export function displayJackets(filteredData) {
   const productWrapper = document.querySelector('.products-wrapper__products');
   productWrapper.innerHTML = '';
 
-  filteredData.map((item) => {
-    const productArticle = document.createElement('article');
-    productArticle.setAttribute('id', item.id);
-    productArticle.classList.add('.products-wrapper__item');
-    productArticle.classList.add('.flex');
-    const content = `
-            <a href="/product/?id=${item.id}">
-            <div class="products-wrapper__img" >
-              <img src="${item.image.url}" alt="${item.description}">
-            </div>
-            <div class="products-product__info-text">
-              <h3>${item.title}</h3>
-              <p>$${item.price}</p>
-            </div>
-            </a>
-      `;
-    productArticle.innerHTML = content;
+  try {
+    filteredData.map((item) => {
+      const productArticle = document.createElement('article');
+      productArticle.setAttribute('id', item.id);
+      productArticle.classList.add('.products-wrapper__item');
+      productArticle.classList.add('.flex');
+      const content = `
+              <a href="/product/?id=${item.id}">
+              <div class="products-wrapper__img" >
+                <img src="${item.image.url}" alt="${item.description}">
+              </div>
+              <div class="products-product__info-text">
+                <h3>${item.title}</h3>
+                <p>$${item.price}</p>
+              </div>
+              </a>
+        `;
+      productArticle.innerHTML = content;
 
-    productWrapper.append(productArticle);
-  });
+      productWrapper.append(productArticle);
+    });
+  } catch (error) {
+    displayMessage(productWrapper, '', error);
+    console.log(error);
+  }
 }
